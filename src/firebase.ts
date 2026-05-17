f4e6fb8802dc7d2f5a8e5def16aee2e5f5cf6bb3
sasadj0511-cmd <sasadj0511@gmail.com>
2026-05-17 06:38:01 +0200

    Fix Firebase Auth initialization - pass app instance to getAuth()

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,4 +8,4 @@ import firebaseConfig from '../firebase-applet-config.json';
 // Initialize Firebase SDK
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth();
+export const auth = getAuth(app);
